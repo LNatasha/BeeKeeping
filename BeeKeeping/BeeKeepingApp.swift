1a19ab92c69a51b39746ac7e18b71aa349e36cde
LNatasha <natasha.louise@gmail.com>
2026-06-29 16:42:18 +0100

    Initial Commit

---
+++ BeeKeeping/BeeKeepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeKeepingApp.swift
+//  BeeKeeping
+//
+//  Created by Natasha on 29/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BeeKeepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
