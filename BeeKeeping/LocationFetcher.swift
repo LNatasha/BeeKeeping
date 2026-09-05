abe9bf19a093bee33db4fabeaeb299afdac142bb
LNatasha <natasha.louise@gmail.com>
2026-09-05 12:50:49 +0200

    Add LocationFetcher and location permission string

---
+++ BeeKeeping/LocationFetcher.swift
@@ -0,0 +1,48 @@
+import CoreLocation
+import Observation
+
+@Observable
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    var coordinate: (latitude: Double, longitude: Double)?
+    var isFetching: Bool = false
+
+    private let manager = CLLocationManager()
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func requestLocation() {
+        isFetching = true
+        let status = manager.authorizationStatus
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.requestLocation()
+        case .denied, .restricted:
+            isFetching = false
+        @unknown default:
+            isFetching = false
+        }
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedWhenInUse || manager.authorizationStatus == .authorizedAlways {
+            manager.requestLocation()
+        } else if manager.authorizationStatus == .denied || manager.authorizationStatus == .restricted {
+            isFetching = false
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        isFetching = false
+        guard let loc = locations.last else { return }
+        coordinate = (latitude: loc.coordinate.latitude, longitude: loc.coordinate.longitude)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        isFetching = false
+    }
+}
